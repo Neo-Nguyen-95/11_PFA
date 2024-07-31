8ae1274be3f5e089257eeeaa1ead1d6d1c32f407
Neo-Nguyen-95 <dzung.usth@gmail.com>
2024-07-31 23:00:56 +0700

    Update EDO

--- EDA.py
+++ EDA.py
@@ -1,6 +1,7 @@
 #%% LIB & IMPORT
 import pandas as pd
 pd.set_option('display.max_columns', None)
+import matplotlib.pyplot as plt
 
 import statsmodels.formula.api as smf
 
@@ -40,6 +41,20 @@ df['Student_ID'] = df['StudentID'].map(student_shorten_id)
 df['Student_ID'] = df['Student_ID'].astype('category')
 df['Skill'] = df['Skill'].astype('category')
 
+# student info
+student_perf = df['Success'].groupby(df['Student_ID']).mean()
+student_frequency = df['Student_ID'].value_counts()
+student_info = pd.concat([student_perf, student_frequency], axis='columns')
+
+plt.figure(dpi=200)
+student_info.plot(kind='scatter', y='Success', x='count')
+
+
+# filter df
+# filter_student_map = (df['Student_ID'].value_counts()>100).to_dict()
+# df['Filter'] = df['Student_ID'].map(filter_student_map)
+# df = df[df['Filter'] == True]
+
 #%% ESTIMATE COEF
 # check crosstab
 crosstab_student_skill = pd.crosstab(df['Student_ID'], df['Skill'])
@@ -47,4 +62,6 @@ crosstab_student_skill.head()
 
 formula = 'Success ~ C(Student_ID) + C(Skill) + Opportunity : C(Skill) - 1'
 log_model = smf.logit(formula, data=df).fit()
+df.to_csv("clean_data.csv")
+
 
