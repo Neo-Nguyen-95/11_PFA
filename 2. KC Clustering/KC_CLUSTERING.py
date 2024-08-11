8fca0b3def5fdb22d59255fed8582f2834ed3a4e
Neo-Nguyen-95 <dzung.usth@gmail.com>
2024-08-11 10:56:29 +0700

    analyze KC

--- 2. KC Clustering/KC_CLUSTERING.py
+++ 2. KC Clustering/KC_CLUSTERING.py
@@ -1,6 +1,14 @@
 #%% LOAD DATA
 import pandas as pd
-import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import silhouette_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.io as pio
+pio.renderers.default="browser"
 
 pd.set_option('display.max_column', None)
 
@@ -18,16 +26,64 @@ def bkt_transform(df):
          'slips': df[df['param']=='slips']['value'].values
          }
         )
+    result.set_index('skill', inplace=True)
     return result
 
 bkt_coef_update = bkt_transform(bkt_coef)
     
-def pfa_transform(df):
-    result = ...
+def pfa_transform(pfa_coef):
+    pfa_coef[['type', 'skill']] = (pfa_coef['Variable']
+                         .str.replace(']','')
+                         .str.split('[', expand=True)
+                         )
+
+    df_skill = pfa_coef.iloc[0:15].set_index('skill')['Coefficient'].rename('skill_diff')
+    df_success = pfa_coef.iloc[15:30].set_index('skill')['Coefficient'].rename('success_coef')
+    df_fail = pfa_coef.iloc[30:45].set_index('skill')['Coefficient'].rename('fail_coef')
+    result = pd.concat([df_skill, df_success, df_fail], axis='columns')
     
     return result
 
-df_skill = pfa_coef.iloc[0:15]
-df_success = pfa_coef.iloc[15:30]
-df_fail = pfa_coef.iloc[30:45]
-        
+pfa_coef_update = pfa_transform(pfa_coef)
+
+#%% CLUSTERING
+def kmean(df):
+    model = make_pipeline(
+        StandardScaler(),
+        KMeans(n_clusters=3, random_state=42)
+        )
+    
+    model.fit(df)
+    
+    df['cluster'] = model.named_steps['kmeans'].labels_
+    
+    cluster_mean = df.groupby('cluster').mean()
+    inertia = model.named_steps['kmeans'].inertia_
+    
+    ss = silhouette_score(df, model.named_steps['kmeans'].labels_)
+    
+    return df, cluster_mean, inertia, ss
+
+#%% BKT coef clustering
+bkt_coef_update, bkt_cluster_mean, bkt_inertia, bkt_ss = kmean(bkt_coef_update)
+plt.figure(dpi=200)
+sns.scatterplot(data=bkt_coef_update, x='prior', y='learns', hue='cluster',
+                palette='pastel')
+
+plt.figure(dpi=200)
+sns.scatterplot(data=bkt_coef_update, x='guesses', y='slips', hue='cluster')
+
+#%% PFA coef clustering
+pfa_coef_update, pfa_cluster_mean, pfa_inertia, pfa_ss = kmean(pfa_coef_update)
+
+fig = px.scatter_3d(pfa_coef_update, z='skill_diff', y='success_coef', x='fail_coef',
+                    color='cluster')
+fig.show()
+
+#%% PFA coef export
+pfa_coef_update.to_csv('PFA_coefficient_clustering.csv')
+pfa_cluster_mean.to_csv('PFA_cluster_parameters.csv')
+
+pfa_coef_update['cluster'] = list(range(len(pfa_coef_update)))
+pfa_coef_update.to_csv('PFA_no_cluster_parameters.csv')
+
---
+++ 2. KC Clustering/KC_SCENARIO.py
@@ -0,0 +1,81 @@
+#%% LOAD DATA
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+
+# pfa_cluster_params = pd.read_csv('PFA_cluster_parameters.csv',
+#                                  index_col='cluster')
+
+pfa_cluster_params = pd.read_csv('PFA_no_cluster_parameters.csv',
+                                 index_col='cluster')
+
+# GENERATE SCENARIO
+table_scenario = pd.DataFrame()
+
+def generate_scenario(df):
+    for cluster in pfa_cluster_params.index:
+        for fail_number in range(4):
+            table = pd.DataFrame({
+                'success_opp': list(range(6)),
+                'fail_opp': [fail_number] * len(range(6)),
+                'cluster': [cluster] * len(range(6))
+                })
+            df = pd.concat([df, table], axis='rows')
+    
+    df['success_fail_distance'] = df['success_opp'] - df['fail_opp']
+
+    df['total_opp'] = df['success_opp'] + df['fail_opp']
+    
+    df = df.reset_index()
+    df.drop(columns='index', inplace=True)
+    
+    return df
+
+table_scenario = generate_scenario(table_scenario)
+
+#%% PFA PREDICT PROB
+def pfa_success_probability(df, df_coef):
+    beta = (df['cluster']
+            .map(lambda x: df_coef.at[x, 'skill_diff'])
+            )
+    
+    gamma = (df['cluster']
+             .map(lambda x: df_coef.at[x, 'success_coef'])
+             )
+    
+    rho = (df['cluster']
+           .map(lambda x: df_coef.at[x, 'fail_coef'])
+           )
+    
+    logit = beta + gamma * df['success_opp'] + rho * df['fail_opp']
+    
+    df['success_probability'] = 1 / (1 + np.exp(-logit))
+    
+    return df
+
+table_scenario = pfa_success_probability(table_scenario, pfa_cluster_params)
+
+# plot
+plt.figure(dpi=200)
+sns.scatterplot(data=table_scenario,
+                x='success_fail_distance', y='success_probability')
+
+#%% DECISION MAKING
+# Success prob if having correct answer at 1st time
+s1 = table_scenario[table_scenario['total_op'] == 0]
+s1
+"""1st time correct => prob > 0.7 => choose 0.8
+"""
+
+# Number of practice should be at least 3, fail at least 1, prob at least 0.8
+s2 = table_scenario[(table_scenario['total_opp'] >= 3) &
+               (table_scenario['success_probability'] >= 0.8)
+               ]
+
+# success - fail distance for each cluster
+
+
+
